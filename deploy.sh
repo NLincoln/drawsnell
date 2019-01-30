3b4f9da985a27e9b31f8237646a18d0a120efd65
Nathan <nlincoln@intellifarms.com>
2019-01-30 08:39:28 -0600

    Solves the hardest problem in computer science
    Revolutionary

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,12 @@ set -e;
 rm -rf build;
 yarn build
 aws s3 cp build s3://drawsnell.com --recursive
+
+echo "=> Invalidating cloudfront caches: changes will still take a few minutes to propogate"
+
+# You know have I ever mentioned how wild aws pricing for cache invalidation is?
+# The pricing is based on _patterns_, not number of things you remove from the cache
+# So the command below basically removes _everything_ in the cache, but it costs basically nothing
+#  because it's only a single pattern!
+
+aws cloudfront create-invalidation --distribution-id EAGGZPDCEA9R7 --paths '/*'
