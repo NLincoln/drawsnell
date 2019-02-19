67628f3c2f3f4d5d89f612542524cf4a3cfe55bf
Nathan LaRose <njl44f@mst.edu>
2019-02-19 11:48:47 -0600

    Backend to import an image

---
+++ src/ImportImage.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class ImportImage extends Component {
+    clickInput() {
+        document.getElementById('file').click();
+    }
+
+    importImage() {
+        var file = document.getElementById('file')
+        var fr = new FileReader();
+        fr.onload = onFileReaderLoad;
+        fr.readAsDataURL(file.files[0]);
+    }
+    
+    render() {
+        return (
+            <div>
+                <button onClick={this.clickInput}>Open File</button>
+                <input type="file" id="file" ref="fileUpload" onChange={this.importImage} style={{display:"none"}}></input>
+            </div>
+        );
+    }
+}
+
+function onFileReaderLoad(e) {
+    var context = document.getElementById('canvas').getContext('2d');
+    var urlStyle = e.target.result;
+    var image = new Image();
+    image.onload = function() {
+        context.drawImage(image, 0, 0);
+    }
+    image.src = urlStyle;
+}
+
+export default Import;
