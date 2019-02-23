9de1e781d402e9d9a7d29231e2cd9dc562f41f14
mjk8t2 <mjk8t2@mst.edu>
2019-02-23 13:47:08 -0600

    Add swap layer z index button to demo

--- src/RightPanel.js
+++ src/RightPanel.js
@@ -45,7 +45,7 @@ export default function RightPanel(props)
     }
     props.changeOneTimeEvent("redrawCanvas");
   }
-  function switchLayers()
+  function swapActiveLayer()
   {
     if(props.activeLayers[0] == 1)
     {
@@ -56,19 +56,33 @@ export default function RightPanel(props)
       props.changeActiveLayers([1])
     }
   }
+  function switchLayersZOrder()
+  {
+    let temp = props.mainComp.layers[1];
+    props.mainComp.layers[1] = props.mainComp.layers[2];
+    props.mainComp.layers[2] = temp;
+    props.changeOneTimeEvent("redrawCanvas");
+  }
   
   
   return <PanelWrapper>Layers, color picker, etc
     <ColorPicker color={props.color} onColorChange={props.onColorChange} />
     
     
-    <button onClick={(e) => switchLayers()}>
-      Switch Layers
+    <button onClick={(e) => swapActiveLayer()}>
+      Swap Active Layers
     </button>
     
     Active Layers = {props.activeLayers}
     
     <br />
+    
+    <button onClick={(e) => switchLayersZOrder()}>
+      Switch Layer Z Order
+    </button>
+    
+    <br />
+    
     <button onClick={(e) => opacityUp()}>
       Opacity Up for layers {props.activeLayers}
     </button>
